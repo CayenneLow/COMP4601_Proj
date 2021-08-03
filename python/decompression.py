125093eda070c905f53ec348af4b06ad86d59cd2
iamadamsrepository <54606556+iamadamsrepository@users.noreply.github.com>
2021-08-03 17:31:34 +1000

    fixed output to conform to output.txt
    also writes to test_output.txt

--- python/decompression.py
+++ python/decompression.py
@@ -1,4 +1,5 @@
 from bisect import insort
+from os import EX_CANTCREAT
 
 class Leaf:
     def __init__(self, char, freq):
@@ -35,7 +36,7 @@ class Branch:
                 stack.append((length+1, node.right))
             else:
                 lengths.append((length, node.char))
-        return sorted(lengths, key=lambda l: l[0])
+        return sorted(lengths, key=lambda l: (l[0]<<32)+l[1])
     
     def __str__(self): return f'Chars: {self.get_chars()}, Freq: {self.freq}'
     
@@ -70,10 +71,16 @@ if __name__ == '__main__':
             codeword = '0' * (prev_length - len(codeword)) + codeword
             if length > prev_length:
                 codeword = codeword + '0' * (length - prev_length)
-        encoding[codeword] = (char, len(codeword))
+        encoding[codeword] = char
         prev = codeword
         prev_length = length
-    for k, v in encoding.items():
-        special = (int(k, base=2) << 5) + len(k)
-        print(v, '\t', hex(special))
+    # for k, v in encoding.items():
+    #     special = (int(k, base=2) << 5) + len(k)
+    #     print(v, '\t', hex(special))
+    encoding = sorted(encoding.items(), key=lambda i: i[1])
+    with open('test_output.txt', 'w') as f:
+        for k, v in encoding:
+            special = (int(k, base=2) << 5) + len(k)
+            print(v, ', ', hex(special)[2:], sep='')
+            f.write(f'{v}, {hex(special)[2:]}\n')
 
